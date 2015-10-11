7c09d22f63cf0b9e22aebfe1b2679900e4dcbae3
bayaba <kevinkim1974@gmail.com>
2015-10-12 03:19:23 +0900

    bayaba's unity example

---
+++ Assets/DrawRope.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawRope : MonoBehaviour
+{
+    LineRenderer RopeLine;
+    
+    void Start()
+	{
+        RopeLine = GetComponent<LineRenderer>();
+        RopeLine.SetVertexCount(transform.childCount);
+    }
+	
+	void Update()
+	{
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            RopeLine.SetPosition(i, transform.GetChild(i).position);
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	void Start()
+	{
+	
+	}
+	
+	void Update()
+	{
+	    if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddRelativeForce(Vector3.left * 60f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddRelativeForce(Vector3.right * 60f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * 3000f);
+        }
+    }
+}
